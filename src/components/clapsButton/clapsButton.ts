dc07570759f94f0ba6b853570403a742f529c643
xananax <me@xananax.com>
2025-03-20 20:50:31 +0100

    chore: share widget improvements

--- src/components/clapsButton/clapsButton.ts
+++ src/components/clapsButton/clapsButton.ts
@@ -1,19 +1,22 @@
 import { Signal } from "../../js/deps.ts";
 
-const clapsOnPage = new Set<[Element, Element]>();
+const clapsOnPage = new Set<[Element, Element, Element]>();
 
 const claps = Signal(0);
+const clapped = Signal(0);
 
 claps.on((amount) => {
   const formattedAmount = formatNumber(amount);
-  clapsOnPage.forEach(([total, abbreviated]) => {
+  clapsOnPage.forEach(([total, abbreviated, bubble]) => {
     total.textContent = `${amount}`;
     abbreviated.textContent = formattedAmount;
+    bubble.textContent = clapped.get() > 0 ? `+${clapped.get()}` : "";
   });
 });
 
 function increase(this: HTMLButtonElement) {
   this.setAttribute("aria-pressed", "true");
+  clapped.set(clapped.get() + 1);
   claps.set(claps.get() + 1);
 }
 
@@ -36,12 +39,13 @@ document
 
     const total = button.querySelector(".clapsButtonAmountTotal");
     const abbreviated = button.querySelector(".clapsButtonAmountAbbreviated");
+    const bubble = button.querySelector(".clapsButtonBubble");
 
-    if (!total || !abbreviated) {
+    if (!total || !abbreviated || !bubble) {
       return;
     }
 
-    clapsOnPage.add([total, abbreviated]);
+    clapsOnPage.add([total, abbreviated, bubble]);
 
     button.addEventListener(
       "animationend",
